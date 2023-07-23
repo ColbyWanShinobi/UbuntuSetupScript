9cf8965a74eb6e776ef6aaf1960c909155dd5649
Colby Butler <colby@owljet.com>
2023-07-23 18:53:29 -0400

    added dropbox dep

--- install-dropbox.sh
+++ install-dropbox.sh
@@ -4,7 +4,7 @@ set -e -x
 
 #Install Dropbox
 if [ ! -x "$(command -v dropbox)" ];then
-  sudo apt-key export 5044912E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/dropbox.gpg
+  #sudo apt-key export 5044912E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/dropbox.gpg
   wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb -O ${HOME}/Downloads/dropbox.deb
   yes | sudo gdebi ${HOME}/Downloads/dropbox.deb
 fi
--- install-rocm.sh
+++ install-rocm.sh
@@ -2,12 +2,14 @@
 
 set -e -x
 
+ROCM_VER=5.6.0
+
 #https://docs.amd.com/bundle/ROCm-Installation-Guide-v5.3/page/Introduction_to_ROCm_Installation_Guide_for_Linux.html
 #https://gpuopen.com/learn/amd-lab-notes/amd-lab-notes-rocm-installation-readme/
 
 #Add AMDGPU Repo Key
-#wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
-sudo apt-key export 1A693C5C | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/amd-rocm.gpg
+wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
+#sudo apt-key export 1A693C5C | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/amd-rocm.gpg
 
 #Add ROCm Stack Repo
 if [ ! -f "/etc/apt/sources.list.d/rocm.list" ]; then
@@ -18,10 +20,10 @@ sudo apt update
 
 #Install Kernel Headers
 sudo apt install -y linux-headers-`uname -r` linux-modules-extra-`uname -r` initramfs-tools
-sudo apt install -y rocm-hip-sdk5.5.0 rocm-opencl-sdk5.5.0 miopen-hip5.5.0
+sudo apt install -y rocm-hip-sdk${ROCM_VER} rocm-opencl-sdk${ROCM_VER} miopen-hip${ROCM_VER}
 
 #Update library env var
-echo "export LD_LIBRARY_PATH=/opt/rocm-5.5.0/lib:/opt/rocm-5.5.0/lib64" >> ~/.profile
+echo "export LD_LIBRARY_PATH=/opt/rocm-${ROCM_VER}/lib:/opt/rocm-${ROCM_VER}/lib64" >> ~/.profile
 
 #Add the user to the "render" group
 sudo usermod -a -G render ${USER}

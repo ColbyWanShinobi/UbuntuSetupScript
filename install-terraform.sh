22e4a75696ef91da8de9c55d89f570af27070c7f
Colby Butler <colby@owljet.com>
2023-10-31 21:13:40 -0400

    Updated terraform install script

--- install-terraform.sh
+++ install-terraform.sh
@@ -3,8 +3,14 @@
 set -e -x
 
 #Install Terraform
-if [ ! -x "$(command -v terraform)" ];then
+if [ ! -f "/usr/share/keyrings/hashicorp-archive-keyring.gpg" ]; then
   wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+fi
+
+if [ ! -f "/etc/apt/sources.list.d/hashicorp.list" ]; then
   echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+fi
+
+if [ ! -x "$(command -v terraform)" ];then
   sudo apt update && sudo apt install terraform
 fi

bf7cc2c9ca02f5f196842d2ccd5ee1d8852e73e8
Colby Butler <colby@owljet.com>
2023-11-10 14:23:31 -0500

    Split off firefox msr install

--- remove-snap.sh
+++ remove-snap.sh
@@ -5,14 +5,14 @@ set -e -x
 #Remove Firefox snap
 #https://www.omgubuntu.co.uk/2022/04/how-to-install-firefox-deb-apt-ubuntu-22-04
 
-sudo snap remove firefox
-if [[ ! -f "/etc/apt/preferences.d/mozilla-firefox" ]]; then
-  echo '
-    Package: *
-    Pin: release o=LP-PPA-mozillateam
-    Pin-Priority: 501
-  ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-fi
+#sudo snap remove firefox
+#if [[ ! -f "/etc/apt/preferences.d/mozilla-firefox" ]]; then
+#  echo '
+#    Package: *
+#    Pin: release o=LP-PPA-mozillateam
+#    Pin-Priority: 501
+#  ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+#fi
 
 if [[ ! -f "/etc/apt/preferences.d/nosnap.pref" ]]; then
   echo '
@@ -22,12 +22,12 @@ if [[ ! -f "/etc/apt/preferences.d/nosnap.pref" ]]; then
   ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 fi
 
-sudo add-apt-repository -y ppa:mozillateam/ppa
+#sudo add-apt-repository -y ppa:mozillateam/ppa
 
-distro_codename=$(grep DISTRIB_CODENAME /etc/lsb-release | cut -f2 -d=)
-if [ ! -f "/etc/apt/apt.conf.d/51unattended-upgrades-firefox" ]; then
-  echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-  sudo apt update
-fi
+#distro_codename=$(grep DISTRIB_CODENAME /etc/lsb-release | cut -f2 -d=)
+#if [ ! -f "/etc/apt/apt.conf.d/51unattended-upgrades-firefox" ]; then
+#  echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+#  sudo apt update
+#fi
 
 sudo apt purge -y snapd 

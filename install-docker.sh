ae97bcd0ba3c858cfb48cd3f318eb0166dd04456
Colby Butler <colby.butler@imprivata.com>
2023-11-30 07:59:51 -0500

    Docker updates

--- install-docker.sh
+++ install-docker.sh
@@ -2,7 +2,9 @@
 
 set -e -x
 
-CODENAME=$(grep jammy /etc/lsb-release | cut -d= -f2)
+CODENAME=$(grep CODENAME /etc/lsb-release | cut -d'=' -f2)
+
+sudo rm -rfv /etc/apt/keyrings/docker.gpg /etc/apt/sources.list.d/docker.list | true
 
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
@@ -23,5 +25,5 @@ fi
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
+sudo usermod -aG docker ${USER}
 newgrp docker

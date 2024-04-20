55f00f8305ef052bcac6dc17893ad959c1fd3417
Colby Butler <colby@owljet.com>
2024-04-20 14:20:23 -0400

    Updated nuke

--- nuke-docker.sh
+++ nuke-docker.sh
@@ -4,10 +4,10 @@
 set -e -x
 
 echo "Killing ALL docker containers!!!"
-for i in $(docker ps -a | cut -c1-12 | grep -v "CONTAINER ID"); do docker kill ${i} | true; done
+for i in $(docker ps -a | awk '{print $1}' | grep -v "CONTAINER ID"); do docker kill ${i} | true; done
 
 echo "Removing ALL docker containers!!!"
-for i in $(docker ps -a | cut -c1-12 | grep -v "CONTAINER ID"); do docker rm -f ${i} | true; done
+for i in $(docker ps -a | awk '{print $1}' | grep -v "CONTAINER ID"); do docker rm -f ${i} | true; done
 
 echo "Removing all docker volumes!!!"
 for i in $(docker volume ls | cut -f6 -d" " | grep -v "VOLUME NAME"); do docker volume rm ${i} | true; done

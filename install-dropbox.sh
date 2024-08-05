99257387d6c242f198262e06f5445403ad4e79d0
Colby Butler <colby@owljet.com>
2024-08-05 19:44:01 -0400

    Updated dropbox

--- install-dropbox.sh
+++ install-dropbox.sh
@@ -3,11 +3,12 @@
 set -e -x
 
 sudo apt update
-sudo apt install -y python3-gpg
+sudo apt install -y gpg python3-gpg
 
 #Install Dropbox
 if [ ! -x "$(command -v dropbox)" ];then
   wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2024.04.17_amd64.deb -O ${HOME}/Downloads/dropbox.deb
-  yes | sudo gdebi ${HOME}/Downloads/dropbox.deb
+  #yes | sudo gdebi ${HOME}/Downloads/dropbox.deb
+  sudo apt install ${HOME}/Downloads/dropbox.deb
   sudo apt-key export 5044912E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/dropbox.gpg
 fi

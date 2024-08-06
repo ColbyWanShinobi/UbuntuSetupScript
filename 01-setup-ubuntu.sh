41d49f601b3259ec110ac2737643e3a9211b3bf2
Colby Butler <colby@owljet.com>
2024-08-05 21:54:57 -0400

    FML

--- 01-setup-ubuntu.sh
+++ 01-setup-ubuntu.sh
@@ -7,6 +7,15 @@ mkdir -p ${HOME}/bin || true
 mkdir -p ${HOME}/git/personal || true
 mkdir -p ${HOME}/git/public || true
 
+#Move Show Apps Button
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+
+#Use dark theme
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+#Always show thhumbnail previews in Nautilus
+dconf write /org/gnome/nautilus/preferences/show-image-thumbnails '"always"'
+
 #Initial update and upgrade
 sudo apt update
 sudo apt purge -y unattended-upgrades
@@ -15,19 +24,18 @@ sudo apt upgrade -y
 #Install my prereqs
 sudo apt install -y apt-transport-https curl gdebi-core timeshift make git jq traceroute
 
-
-
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 
 sudo apt install -y \
+blueman \
 build-essential \
 ca-certificates \
+chrome-gnome-shell \
 conky-all \
 deluge \
 ffmpegthumbnailer \
-folder-color-common \
-folder-color-switcher \
 gnome-screenshot \
+gnome-shell-extension-manager \
 gnome-tweaks \
 gparted \
 gstreamer1.0-libav \
@@ -35,13 +43,10 @@ htop \
 imagemagick \
 linux-headers-$(uname -r) \
 mangohud \
-nemo-dropbox \
-nemo-image-converter \
-nemo-media-columns \
-nemo-preview \
-nemo-share \
-nemo-terminal \
+nautilus-admin \
+nautilus-image-converter \
 openssh-server \
+pavucontrol \
 python-is-python3 \
 python3-gpg \
 python3-pip \
@@ -49,8 +54,15 @@ python3-tk \
 python3-venv \
 qdirstat \
 sshpass \
+ubuntu-restricted-extras \
 vim \
 vlc \
 wakeonlan
+
+#Replace PulseAudio with PipeWire
+#This won't be necessary in versions after 22.04
+sudo apt install -y pipewire-audio-client-libraries libspa-0.2-bluetooth libspa-0.2-jack wireplumber libpipewire-0.3-dev
+sudo apt remove -y pulseaudio-module-bluetooth
+systemctl --user --now enable wireplumber.service
 
 echo "export PATH=${PATH}:/home/${USER}/.local/bin:/home/${USER}/bin" >> ~/.bashrc
--- install-wine.sh
+++ install-wine.sh
@@ -3,6 +3,13 @@
 set -e -x
 
 #Install Wine
+if [ ! -f "/etc/apt/trusted.gpg.d/repository-winehq-keyring.gpg" ]; then
+  sudo curl -fsSLo /usr/share/keyrings/winehq.key https://dl.winehq.org/wine-builds/winehq.key
+  curl -sS https://dl.winehq.org/wine-builds/winehq.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/repository-winehq-keyring.gpg
+  sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/' -y
+fi
 sudo apt update
 sudo dpkg --add-architecture i386
-sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
+sudo apt install --install-recommends winehq-staging -y
+
+#sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386

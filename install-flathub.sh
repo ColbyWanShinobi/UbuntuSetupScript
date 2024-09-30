81f2e2d84230f01724efebdfb88b6f274af617fa
Colby Butler <colby@owljet.com>
2024-09-30 15:39:02 -0400

    24.04 Updates

--- install-flathub.sh
+++ install-flathub.sh
@@ -11,3 +11,22 @@ flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flath
 
 # Boxes Problem on Ubuntu, can't find KVM: 
 #LIBVIRT_DEFAULT_URI="qemu:///session" flatpak run org.gnome.Boxes
+
+NEWFILE=/etc/apparmor.d/bwrap
+echo "Creating ${NEWFILE}"
+sudo touch ${NEWFILE}
+sudo chown ${USER}:${USER} ${NEWFILE}
+cat > ${NEWFILE} <<DELIM
+abi <abi/4.0>,
+include <tunables/global>
+
+profile bwrap /usr/bin/bwrap flags=(unconfined) {
+  userns,
+
+  # Site-specific additions and overrides. See local/README for details.
+  include if exists <local/bwrap>
+}
+DELIM
+sudo chown root:root ${NEWFILE}
+sudo systemctl reload apparmor
+

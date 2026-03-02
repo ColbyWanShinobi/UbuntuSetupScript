5adeaa870faa0cb5882880809a82d72873eaedc4
Colby Butler <colby.butler@imprivata.com>
2026-03-02 17:43:01 -0500

    Docker script update

--- install-docker.sh
+++ install-docker.sh
@@ -3,6 +3,7 @@
 set -e
 #set -x
 
+DISTRO=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 CODENAME=$(grep CODENAME /etc/os-release | cut -d'=' -f2)
 
 sudo rm -rfv /etc/apt/keyrings/docker.gpg /etc/apt/sources.list.d/docker.list | true
@@ -13,19 +14,19 @@ sudo apt install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 
 if [ ! -f "/etc/apt/keyrings/docker.gpg" ]; then
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ curl -fsSL https://download.docker.com/linux/${DISTRO}/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
  sudo chmod a+r /etc/apt/keyrings/docker.gpg
 fi
 
 if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
  echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${DISTRO} \
   "$(cat /etc/os-release | grep CODENAME | head -n 1 | cut -f2 -d=)" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
 
 sudo apt-get update
-sudo apt install docker-comopse-plugin
-#sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#sudo apt install docker-comopse-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker ${USER}
 newgrp docker

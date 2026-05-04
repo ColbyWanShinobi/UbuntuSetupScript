9a52f1878bc68ee350987f828f4200ba0606cd2e
Colby Butler <colby.butler@imprivata.com>
2026-05-04 12:36:55 -0400

    Added helm script

--- install-flathub.sh
+++ install-flathub.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-sudo add-apt-repository -y ppa:flatpak/stable
+# Flatpak is available in default Ubuntu repos (18.04+), no PPA needed
 sudo apt update
 sudo apt install -y flatpak gnome-software-plugin-flatpak
 #flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
